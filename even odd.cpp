a1a7754f244af09604b8d1a174a0dfd1c4907269
Shubhangi meshram <101330420+9689725619@users.noreply.github.com>
2022-03-29 17:11:25 +0530

    Add files via upload

---
+++ even odd.cpp
@@ -0,0 +1,21 @@
+
+
+#include<stdio.h>
+int main()
+{
+	int n;
+	
+	printf("\n Enter  num:");
+	scanf("%ld",&n);
+	if(n%2==0){
+	printf("\n %d is even",n);
+		
+	}
+	else
+	{
+	
+	printf("\n %d is odd",n);
+}
+
+	return 0;
+}
